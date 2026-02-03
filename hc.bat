ed5aa9a0666e866a576cc4a60068d929da34ef81
HeadySystems <heady@systems.local>
2026-02-03 15:41:05 -0700

    HeadyHive Evolution [2026-02-03 15:40:58]: Updates to M hc.bat

--- hc.bat
+++ hc.bat
@@ -1,26 +1,4 @@
 @echo off
-<<<<<<< C:/Users/erich/Heady/hc.bat
-<<<<<<< C:/Users/erich/Heady/hc.bat
-<<<<<<< C:/Users/erich/Heady/hc.bat
-<<<<<<< C:/Users/erich/Heady/hc.bat
-<<<<<<< C:/Users/erich/Heady/hc.bat
-<<<<<<< C:/Users/erich/Heady/hc.bat
-<<<<<<< C:/Users/erich/Heady/hc.bat
-cd /d "C:\Users\erich\Heady"
-powershell -ExecutionPolicy Bypass -File "C:\Users\erich\Heady\scripts\Invoke-Checkpoint.ps1" %*
-=======
-=======
->>>>>>> C:/Users/erich/.windsurf/worktrees/Heady/Heady-e3f3d3cd/hc.bat
-=======
->>>>>>> C:/Users/erich/.windsurf/worktrees/Heady/Heady-e3f3d3cd/hc.bat
-=======
->>>>>>> C:/Users/erich/.windsurf/worktrees/Heady/Heady-20681cb5/hc.bat
-=======
->>>>>>> C:/Users/erich/.windsurf/worktrees/Heady/Heady-e3f3d3cd/hc.bat
-=======
->>>>>>> C:/Users/erich/.windsurf/worktrees/Heady/Heady-e3f3d3cd/hc.bat
-=======
->>>>>>> C:/Users/erich/.windsurf/worktrees/Heady/Heady-e3f3d3cd/hc.bat
 setlocal
 set "SCRIPT_DIR=%~dp0"
 cd /d "%SCRIPT_DIR%"
